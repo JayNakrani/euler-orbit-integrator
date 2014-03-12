3eeb65a534d96f20b38998432a43f5a6226047b8
Dhananjay Nakrani <dhananjaynakrani@gmail.com>
2014-03-12 11:16:28 +0530

    Use Normalized values in pow(). Replace double with long doubles.

--- src/eular_integrator.c
+++ src/eular_integrator.c
@@ -2,36 +2,37 @@
 #include <math.h>
 
 #define DAY_TO_SEC(DT) (DT * 24 * 3600)
+#define NORMALIZE_POS(A) (A/1E+08)
 
 typedef struct struct_position_vector
 {
-	double x,y,z;
+	long double x,y,z;
 }position_vector;
 
 typedef struct struct_velocity_vector
 {
-	double vx,vy,vz;
+	long double vx,vy,vz;
 }velocity_vector;
 
 inline void print_vectors(position_vector a,velocity_vector b,double t)
 {
 	printf("t:%06.2lf",t);
-	printf("\tx:%01.12E\ty:%01.12E\tz:%01.12E",a.x,a.y,a.z);
-	printf("\t vx:%01.12E\tvy:%01.12E\tvz:%01.12E\n",b.vx,b.vy,b.vz);
+	printf("\tx:%01.12LE\ty:%01.12LE\tz:%01.12LE",a.x,a.y,a.z);
+	printf("\t vx:%01.12LE\tvy:%01.12LE\tvz:%01.12LE\n",b.vx,b.vy,b.vz);
 }
 
 int main(void)
 {
 	position_vector cur_pv,next_pv; // position vectors
 	velocity_vector cur_vv,next_vv; // velocity vectors
-	double cur_t = 0;	//current time
-	double finish_time = 30 * 365; // total number of days. 30 years multiplied by 365 days a year. Ignoring leap years for now..!!
-	double dt = 0.1;	// delta-t in number of days
-	// We’ll use units of solar masses, and Astronomical Units for distance
-		double G = 39.47;
-		double M_sun = 1;
+	long double cur_t = 0;	//current time
+	long double finish_time = 30 * 365; // total number of days. 30 years multiplied by 365 days a year. Ignoring leap years for now..!!
+	long double dt = 0.1;	// delta-t in number of days
+	// constants
+	const long double G = 6.67384E-17;
+	const long double M_sun = 1.9891E+30;
 	// misc
-		double tmp = 0;
+		long double tmp = 0;
 	
 	// initialize
 		cur_pv.x = 5.096210785220794E+08;
@@ -60,7 +61,7 @@ int main(void)
 				next_pv.y = cur_pv.y + (cur_vv.vy * DAY_TO_SEC(dt));
 				next_pv.z = cur_pv.z + (cur_vv.vz * DAY_TO_SEC(dt));
 			// compute velocity vector 
-				tmp = sqrtl(powl( (powl(cur_pv.x,2)+powl(cur_pv.y,2)+powl(cur_pv.z,2)),3));
+				tmp = sqrtl(powl( (powl(NORMALIZE_POS(cur_pv.x),2)+powl(NORMALIZE_POS(cur_pv.y),2)+powl(NORMALIZE_POS(cur_pv.z),2)),3)) * 1E+04;	// Use of normalized values for powering.
 				next_vv.vx = cur_vv.vx - (G * M_sun * cur_pv.x / tmp );
 				next_vv.vy = cur_vv.vy - (G * M_sun * cur_pv.y / tmp );
 				next_vv.vz = cur_vv.vz - (G * M_sun * cur_pv.z / tmp );
